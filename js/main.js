27b292f6f8aa933416f1d0622117f05467a645f6
Mattheww <maksi.udalykh@mail.ru>
2023-05-03 05:14:15 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 function random(x, y) {
-<<<<<<< HEAD
-  const z = x - 0.5 + Math.random() * (y - x + 1);
-  return Math.round(z);
- }
- random(10, 110);
- //
-
- //Функция для проверки максимальной длины строки
- function MaxLine(str,maxlong) {
-   return str.length<=maxlong;
- }
- MaxLine('Пример коментария',140);
- //
-=======
    const z = x - 0.5 + Math.random() * (y - x + 1);
    return Math.round(z);
   }
@@ -26,4 +12,3 @@ function random(x, y) {
   }
   MaxLine('Пример коментария',140);
   //
->>>>>>> 31ab9a7a98167559f4291bcbbf122e7bcdbfd737
